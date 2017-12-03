df2a33379884c76fc53ac81d0b189d9b54f71613
VVNoodle <brickkace@gmail.com>
2017-12-03 14:51:21 -0800

    now users can ACTUALLY make and use shorten URLS! Functionally, this microservice is working

--- server.js
+++ server.js
@@ -53,6 +53,30 @@ app.get("/new/:targetURL(*)", (req, res) => {
   }
 });
 
+// use shorten URL
+app.get("/:shortenURL(*)", (req, res) => {
+  let { shortenURL } = req.params;
+  url
+    .findOne({ shortenURL })
+    .then(url => {
+      if (!url) {
+        return res.send(
+          "This shorten URL doesn't correspond to any original URL"
+        );
+      }
+      if (
+        url.originalURL.slice(0, 7) == "http://" ||
+        url.originalURL.slice(0, 8) == "https://"
+      ) {
+        return res.redirect(url.originalURL);
+      }
+      res.redirect("http://" + url.originalURL);
+    })
+    .catch(err => {
+      return new Error("Can't redirect to website");
+    });
+});
+
 // listen to see port
 app.listen(process.env.PORT, () => {
   console.log(`Listening to port ${process.env.PORT}...`);
